92c0732e5d9646e996ec8fbeb67faf3e4094d710
Bruno Goncalves <bigbruno@gmail.com>
2020-04-15 14:08:14 -0300

    ok

---
+++ biglinux-deb-installer/usr/share/bigbashview/bcc/apps/biglinux-deb-installer/broken_solve_terminal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Install .deb packages
+#           
+#Author Bruno Goncalves  <www.biglinux.com.br>
+#License: GPLv2 or later                       
+#################################################
+
+
+PID_BIG_DEB_INSTALLER="$$" WINDOW_ID="$WINDOW_ID" ./broken_solve_terminal_resize.sh &
+
+apt -y -f install
+
+
---
+++ biglinux-deb-installer/usr/share/bigbashview/bcc/apps/biglinux-deb-installer/install_terminal.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Install .deb packages
+#           
+#Author Bruno Goncalves  <www.biglinux.com.br>
+#License: GPLv2 or later                       
+#################################################
+
+
+PID_BIG_DEB_INSTALLER="$$" WINDOW_ID="$WINDOW_ID" ./install_terminal_resize.sh &
+
+OIFS=$IFS
+IFS=$'\n'
+
+rm -f /tmp/biglinux-deb-installer-error
+
+if [ "$APT_UPDATE" = "1" ]; then
+    apt update
+else
+    apt -y install $REINSTALL $(cat /tmp/biglinux-deb-installer-list) 2> >(tee /tmp/biglinux-deb-installer-error)
+fi
+
+if [ "$(xwininfo -id $WINDOW_ID 2>&1 | grep -i "No such window")" != "" ]; then
+    kill -9 $PID_BIG_DEB_INSTALLER
+    exit 0
+fi
+
+IFS=$OIFS
